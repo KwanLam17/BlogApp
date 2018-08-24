54acfc94ae7cf76577b6d7529b397add5051d81c
KwanLam <kwan.lam@alumni.ubc.ca>
2018-08-23 23:28:39 -0700

    FIFTH

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,11 +1,44 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+import { Link } from 'react-router-dom';
 
-export default class PostsIndex extends Component {
+class PostsIndex extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                </li>
+            )
+        })
+    }
+
     render() {
+        //console.log(this.props.posts);
         return (
             <div>
-                Posts Index
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Post
+                    </Link>
+                </div>
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         )
     };
 }
+
+function mapStateToProps(state) {
+    return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex); // mapDispatchToProps done by connect automatically
